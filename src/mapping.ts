e14bb1c2fa1a149083dd9a5ff51dafc2d07fea2c
CarlosR <carlos.ramos@grupoimportia.com>
2022-11-05 12:00:33 -0700

    INIT : first version of graph using template

---
+++ src/mapping.ts
@@ -0,0 +1,30 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+// import {
+//   timestampToDay
+// } from "./helpers"
+import {
+  transactionSubmitted
+} from "../generated/JomTx/JomTx"
+import { Transaction } from "../generated/schema"
+
+export function handleTransactionSubmitted( event : transactionSubmitted) : void {
+
+  let entity = new Transaction(event.transaction.hash.toHex());
+
+  entity.StoreNullifier = event.params.identityCommitment;
+  entity.ipfsURI = event.params.ipfs_uri;
+  entity.buyerAddr = event.params.buyer_addr.toHex();
+  entity.save();
+
+  // let providerEntity = Provider.load(event.transaction.from.toHex());
+  // if(providerEntity){
+  //   let tmp = providerEntity.ConnectorIndexes;
+  //   tmp.push(entity.ConnectorIndex);
+  //   providerEntity.ConnectorIndexes = tmp;
+  //   providerEntity.save()
+  // } 
+
+
+}
+
+
